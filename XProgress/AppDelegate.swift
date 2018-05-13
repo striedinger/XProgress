f337a7c5f88601020b22775572f629224bcf91e0
Hugo Striedinger <striedinger@hugos-macbook-pro.local>
2018-05-13 14:50:06 -0400

    Initial Commit

---
+++ XProgress/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  XProgress
+//
+//  Created by Hugo Striedinger on 5/13/18.
+//  Copyright © 2018 Hugo Striedinger. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ XProgress/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  XProgress
+//
+//  Created by Hugo Striedinger on 5/13/18.
+//  Copyright © 2018 Hugo Striedinger. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
